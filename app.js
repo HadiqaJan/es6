c62203cbe8e72a7657dfdfc80a6ca16ed6df7599
HadiqaJan <86835918+HadiqaJan@users.noreply.github.com>
2021-09-25 19:37:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+
+//1. write a program which shows the couter after each second.
+
+// function counter() {
+//     var date = new Date()
+//     var sec = date.getSeconds();
+//     console.log(sec)
+// }
+// setInterval(counter,1000)
+
+
+
+
+//2. write a function which takes your name and displays the greeting with your name
+
+// greeting=()=>{
+//     nam = prompt("Enter name :")
+//     console.log("Hello " + nam)
+// } 
+// greeting()
+
+
+
+//3. write a function(arrow function) which takes two values and return its sum as a result
+
+// sum = (num1,num2) =>{ num1 +num2}
+// console.log(sum(3,5))
+
+
+
+
+//4.write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+
+// num =(val) => {
+//  return val*0.5;
+// }
+// console.log(num(4))
+
+
+
+//5.print simple array of [1,2,3,4,5] with the help of array map funtion
+
+// let arr =[1,2,3,4,5]
+// let AR= arr.map((i)=>++i)
+// console.log(AR)
+
+
+
+//6. let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+
+// let arr = [
+//     {id:1,name:"abc"},
+//     {id:1,name:"efg"},
+//     {id:2,name:"hij"},
+//     {id:3,name:"xyz"}
+// ]
+
+// arr.map((i)=>{
+//     console.log(i.id +" : " + i.name)
+// })
